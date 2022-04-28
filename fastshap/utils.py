54597b060b68aff01025566c5576b4f487ec6c78
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2022-04-28 13:46:18 -0400

    cifar_visualization_v1: Visualization part working

--- fastshap/utils.py
+++ fastshap/utils.py
@@ -5,7 +5,22 @@ import itertools
 from torch.utils.data import Dataset
 from torch.distributions.categorical import Categorical
 
-from criterions import LabelSmoothingCrossEntropyLoss
+class LabelSmoothingCrossEntropyLoss(nn.Module):
+    def __init__(self, classes, smoothing=0.0, dim=-1):
+        super(LabelSmoothingCrossEntropyLoss, self).__init__()
+        self.confidence = 1.0 - smoothing
+        self.smoothing = smoothing
+        self.cls = classes
+        self.dim = dim
+
+    def forward(self, pred, target):
+        pred = pred.log_softmax(dim=self.dim)
+        with torch.no_grad():
+            true_dist = torch.zeros_like(pred)
+            true_dist.fill_(self.smoothing / (self.cls - 1))
+            true_dist.scatter_(1, target.data.unsqueeze(1), self.confidence)
+        return torch.mean(torch.sum(-true_dist * pred, dim=self.dim))
+
 
 class MaskLayer1d(nn.Module):
     '''
@@ -80,7 +95,7 @@ class KLDivLoss(nn.Module):
 # mod
 class CELoss(nn.Module):
     '''
-    Cross Entropy loss that 
+    Cross Entropy loss that
     smoothing < 0: no smoothing is applied
     '''
     def __init__(self, smoothing=-1, num_classes=10):
